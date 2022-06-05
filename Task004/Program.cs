9f9d1b33dc63c02f803a38a8b3ddca51863edbf6
Nikita Kozlov <kozlov.nu@gmail.com>
2022-06-05 13:29:33 +0300

    Add task4 while

---
+++ Task004/Program.cs
@@ -0,0 +1,32 @@
+﻿// 
+
+Console.WriteLine("Enter your number: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+//int numberB = 0;
+int count = - numberA;
+Console.Write(count);
+Console.Write(", ");
+int numberB = - numberA;
+
+if (numberA >0)
+{
+    while (count < (numberA-1))
+    {
+        numberB = numberB + 1;
+        Console.Write(numberB);
+        Console.Write(", ");
+        count += 1;
+    }
+    Console.WriteLine(numberA);
+}
+else
+{
+ while (count > (numberA+1))
+    {
+        numberB = numberB - 1;
+        Console.Write(numberB);
+        Console.Write(", ");
+        count -= 1;
+    }
+    Console.WriteLine(numberA);   
+}
