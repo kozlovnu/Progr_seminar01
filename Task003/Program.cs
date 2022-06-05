2aaebddea15efc8b3fb1887c03bb182d88eaab51
Nikita Kozlov <kozlov.nu@gmail.com>
2022-06-05 12:42:34 +0300

    Add task number 3 week days

---
+++ Task003/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.Write("Enter the number of a day from 1 to 7: ");
+int user_number = Convert.ToInt32(Console.ReadLine());
+
+if (user_number == 1)
+{
+    Console.WriteLine("It's Monday");
+}
+if (user_number == 2)
+{
+    Console.WriteLine("It's Tuesday");
+}
+if (user_number == 3)
+{
+    Console.WriteLine("It's Wednesday");
+}
+if (user_number == 4)
+{
+    Console.WriteLine("It's Thursday");
+}
+if (user_number == 5)
+{
+    Console.WriteLine("It's Friday");
+}
+if (user_number == 6)
+{
+    Console.WriteLine("It's Saturday");
+}
+if (user_number == 7)
+{
+    Console.WriteLine("It's Sunday");
+}
