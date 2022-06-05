1622bec9f4afaa050dbada29092a27481714e406
Nikita Kozlov <kozlov.nu@gmail.com>
2022-06-05 13:51:36 +0300

    Add the second Extra task convert numbers

---
+++ Extra002/Program.cs
@@ -0,0 +1,8 @@
+﻿// Напишите программу, которая на вход принимает значение, а на выходе показывает обратное значение.
+
+Console.WriteLine("Enter your number: ");
+double numberA = Convert.ToDouble(Console.ReadLine());
+
+numberA = 1 / numberA;
+Console.Write("Converted number = ");
+Console.WriteLine(numberA);
