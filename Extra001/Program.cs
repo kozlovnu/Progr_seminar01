3e4f8068f4a88b9f942fc98988ea1ecf33dfaab5
Nikita Kozlov <kozlov.nu@gmail.com>
2022-06-05 13:38:03 +0300

    Add extra task module

---
+++ Extra001/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу вычисления модуля числа. 
+// 2 -> 2
+//-3 -> 3
+// -7 -> 7
+
+Console.WriteLine("Enter your number: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+if (numberA > 0)
+{
+    Console.Write("module of your number = ");
+    Console.WriteLine(numberA);
+}
+else 
+{ 
+    Console.Write("module of your number = ");
+    Console.WriteLine(-numberA);
+}
