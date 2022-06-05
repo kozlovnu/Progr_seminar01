d1236c23c0174cf50ca1f302377ee97cac7a5f73
Nikita Kozlov <kozlov.nu@gmail.com>
2022-06-05 12:28:38 +0300

    Add task number 2 if-else

---
+++ Task002/Program.cs
@@ -0,0 +1,17 @@
+﻿//Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго
+
+Console.WriteLine("Enter the first number: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter the second number: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 / number2 == number2)
+{
+    Console.WriteLine("First number is a square of the second");
+}
+else 
+{
+    Console.WriteLine("First number is NOT the square of the second number");
+}
+    
+
