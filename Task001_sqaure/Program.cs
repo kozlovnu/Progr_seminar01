73529857d522064b88e6a6af2bf49419ca42444a
Nikita Kozlov <kozlov.nu@gmail.com>
2022-06-05 11:50:29 +0300

    Add first task square of the number

---
+++ Task001_sqaure/Program.cs
@@ -0,0 +1,7 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт его квадрат (число умноженное на само себя).
+
+Console.WriteLine("Enter your number: ");
+int number = Convert.ToInt32(Console.ReadLine()); // Convert.ToInt32 конвертирование строки в число
+int square = number * number;
+Console.Write("Square of the number = ");
+Console.WriteLine(square);
